d9755d0d01d604533752bb97bc5b5913464507e7
harshoberoi <121999815+harshoberoi@users.noreply.github.com>
2023-08-18 15:34:42 +0530

    Solved

---
+++ Beginners Contest/ABC100/A - Happy Birthday!.c
@@ -0,0 +1,15 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    
+    int A,B;
+scanf("%d %d",&A,&B);
+if(A<=8 && B<=8){
+    printf("Yay!");
+}
+else
+printf(":(");
+
+    return 0;
+}
